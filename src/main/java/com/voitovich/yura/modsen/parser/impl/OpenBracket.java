57fd69475ebe17fe6435566d184332ceefada11d
Yura Voitovich <voitovich200233@gmail.com>
2023-06-10 03:49:10 +0300

    Added brackets lexemes

---
+++ src/main/java/com/voitovich/yura/modsen/parser/impl/OpenBracket.java
@@ -0,0 +1,21 @@
+package com.voitovich.yura.modsen.parser.impl;
+
+import com.voitovich.yura.modsen.parser.Lexeme;
+import com.voitovich.yura.modsen.parser.Operator;
+import com.voitovich.yura.modsen.parser.Priority;
+
+import java.util.Optional;
+
+public class OpenBracket implements Operator {
+
+    private final String regex = "(";
+    @Override
+    public Optional<Lexeme> parse(String stringToParse) {
+        return stringToParse.trim().equals(regex) ? Optional.of(this) : Optional.empty();
+    }
+
+    @Override
+    public Priority getPriority() {
+        return Priority.LOWEST;
+    }
+}
